dea230442fe7f844d29f562a5bf136086ef7b155
shruti.singh <shruti.singh@servicenow.com>
2025-02-22 15:13:30 +0530

    updated

--- sample2/src/main/java/hello/Greeter.java
+++ sample2/src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Welcome to my project- Shruti";
+        return "Welcome to my project- Shruti test";
     }
 }
