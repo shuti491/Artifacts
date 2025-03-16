bce6c244f791e876694825722fb357c91e7766fd
shruti.singh <shruti.singh@servicenow.com>
2025-03-16 15:05:38 +0530

    Created dependency project

---
+++ PrjDep1/src/main/java/dep1/Dep1.java
@@ -0,0 +1,11 @@
+package dep1;
+
+public class Dep1 {
+	 public static void main(String[] args) {
+		 System.out.println("Dependency 1 Loaded");
+	 }
+	 
+//    public void run() {
+//        System.out.println("Dependency 1 Loaded");
+//    }
+}
