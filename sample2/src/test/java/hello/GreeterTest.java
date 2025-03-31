3bec53809c0b1d52d48579a537b4c83813d455b3
shruti.singh <shruti.singh@servicenow.com>
2025-03-31 17:21:13 +0530

    updated files

--- sample2/src/test/java/hello/GreeterTest.java
+++ sample2/src/test/java/hello/GreeterTest.java
@@ -1,6 +1,14 @@
 package hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class GreeterTest {
@@ -12,5 +20,65 @@ class GreeterTest {
         System.out.println(result);
         
         assertEquals("Main Project Running...", result, "Greeting message should be 'Main Project Running..'");
+    }
+    
+    @Test
+    public void testConvertToDoubleOK() {
+      for (int i = 0; i < 1000000; i++) {
+        String age = "1990";
+        Double expAge = Double.valueOf(age);
+        Double actual = StringUtils.convertToDouble(age);
+        assertNotNull(actual);
+        assertEquals(expAge, actual);
+      }
+    }
+
+    @Test
+    public void testConvertToDoubleWithNullArgument() {
+      for (int i = 0; i < 500000; i++) {
+        String actual = null;
+        assertNull(actual, "The actual is not null");
+      }
+    }
+
+    @Test
+    public void testConvertToDoubleThrowException() {
+      for (int i = 0; i < 200000; i++) {
+        String age = "N/A";
+        assertThrows(NumberFormatException.class, () -> {
+          StringUtils.convertToDouble(age);
+        });
+      }
+    }
+
+    @Test
+    public void testIsNullOrBlankOK() {
+      for (int i = 0; i < 500000; i++) {
+        assertTrue(StringUtils.isNullOrBlank(null));
+        assertTrue(StringUtils.isNullOrBlank(" "));
+        assertFalse(StringUtils.isNullOrBlank("abc"));
+      }
+    }
+
+    @Test
+    public void testGetDefaultIfNull() {
+      for (int i = 0; i < 200000; i++) {
+        assertSame("abc", StringUtils.getDefaultIfNull(null, "abc"));
+        assertNotSame("abc", StringUtils.getDefaultIfNull("def", "abc"));
+      }
+    }
+
+    @Test
+    public void testConcatWithRegularInput() {
+      for (int i = 0; i < 200000; i++) {
+        assertEquals("HelloWorld!", StringUtils.concat("Hello", "World", "!"));
+      }
+    }
+
+    @Test
+    public void testConcatWithNullInput() {
+      for (int i = 0; i < 200000; i++) {
+        assertEquals("HelloWorld", StringUtils.concat("Hello", "World", null));
+      }
     }
 }
--- sample2/src/test/java/hello/HelloWorldTest.java
+++ sample2/src/test/java/hello/HelloWorldTest.java
@@ -2,11 +2,41 @@ package hello;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Disabled;
 
 class HelloWorldTest {
 
     @Test
     void testMainMethod() {
         assertDoesNotThrow(() -> HelloWorld.main(new String[]{}), "Main method should run without exceptions");
+    }
+    @Test
+    public void testConcatWithRegularInput() {
+      String st1 = "Hello";
+      String st2 = "World";
+      String st3 = "!";
+      String expect = st1 + st2 + st3;
+      String actual = StringUtils.concat(st1, st2, st3);
+      assertEquals(expect, actual);
+    }
+
+    @Disabled
+    @Test
+    public void testConcatWithNullInput() {
+      String st1 = "Hello";
+      String st2 = "World";
+      String st3 = null;
+      String expect = st1 + st2;
+      String actual = StringUtils.concat(st1, st2, st3);
+      assertEquals(expect, actual);
+    }
+
+    @Test
+    public void testConcatWithAllNullInput() {
+      String actual = StringUtils.concat();
+      assertNull(actual);
     }
 }
